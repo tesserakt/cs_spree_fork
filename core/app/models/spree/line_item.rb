407da153076643863d73db177299cc2aa50a6bef
tesserakt <tesserakt@users.noreply.github.com>
2016-01-15 15:26:40 -0500

    Update line_item.rb
    Speed up the order merge! call which calls order.destroy and Rails uses the line_item relationship to delete the inventory units.

--- core/app/models/spree/line_item.rb
+++ core/app/models/spree/line_item.rb
@@ -8,7 +8,7 @@ module Spree
     has_one :product, through: :variant
 
     has_many :adjustments, as: :adjustable, dependent: :destroy
-    has_many :inventory_units, inverse_of: :line_item
+    has_many :inventory_units, inverse_of: :line_item, dependent: :delete_all
 
     before_validation :copy_price
     before_validation :copy_tax_category
